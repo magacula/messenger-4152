ea2a7e1dfb38128823e968351109202c2e24e4c6
magacula <makoigacula@gmail.com>
2021-10-30 00:27:10 -0700

    fixed displaying correct order of messages on page load

--- client/src/components/ActiveChat/ActiveChat.js
+++ client/src/components/ActiveChat/ActiveChat.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Box } from "@material-ui/core";
 import { Input, Header, Messages } from "./index";
@@ -8,7 +8,7 @@ const useStyles = makeStyles(() => ({
   root: {
     display: "flex",
     flexGrow: 8,
-    flexDirection: "column"
+    flexDirection: "column",
   },
   chatContainer: {
     marginLeft: 41,
@@ -16,14 +16,31 @@ const useStyles = makeStyles(() => ({
     display: "flex",
     flexDirection: "column",
     flexGrow: 1,
-    justifyContent: "space-between"
-  }
+    justifyContent: "space-between",
+  },
 }));
+
+// function that sorts messages by id in ascending order on page load
+const sortById = (messagesArr) => {
+  // sorts messages by id (newer messages are at the beginning of array)
+  return messagesArr.sort((a, b) => {
+    return a.id - b.id;
+  });
+};
 
 const ActiveChat = (props) => {
   const classes = useStyles();
   const { user } = props;
   const conversation = props.conversation || {};
+
+  // uses useMemo hook that caches a sorted message array, optimizing performance
+  // displays messages with oldest messages at the top and newest messages at the bottom
+  const displayMessages = useMemo(() => {
+    // executes sort function and returns message array only after clicking on an active conversation
+    if (conversation.messages !== undefined) {
+      return sortById(conversation.messages);
+    }
+  }, [conversation.messages]);
 
   return (
     <Box className={classes.root}>
@@ -35,7 +52,7 @@ const ActiveChat = (props) => {
           />
           <Box className={classes.chatContainer}>
             <Messages
-              messages={conversation.messages}
+              messages={displayMessages}
               otherUser={conversation.otherUser}
               userId={user.id}
             />
@@ -57,8 +74,9 @@ const mapStateToProps = (state) => {
     conversation:
       state.conversations &&
       state.conversations.find(
-        (conversation) => conversation.otherUser.username === state.activeConversation
-      )
+        (conversation) =>
+          conversation.otherUser.username === state.activeConversation
+      ),
   };
 };
 
