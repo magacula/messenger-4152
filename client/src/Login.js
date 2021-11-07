c0512b598459c09382bd632c94226d13cee36738
magacula <makoigacula@gmail.com>
2021-11-06 17:43:05 -0700

    added custom components to Login / Signup

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,23 +1,35 @@
 import React from "react";
 import { Redirect, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
-import {
-  Grid,
-  Box,
-  Typography,
-  Button,
-  FormControl,
-  TextField,
-} from "@material-ui/core";
+import { Grid, Box, Typography, Button } from "@material-ui/core";
 import { login } from "./store/utils/thunkCreators";
 import { useStyles } from "./styles/styles";
-import backgroundImg from "./assets/bg-img.png";
-import { ReactComponent as ChatLogo } from "./assets/bubble.svg";
+import { Form } from "./components/Form";
+import { SideBanner } from "./components/SideBanner";
 
 const Login = (props) => {
   const history = useHistory();
   const { user, login } = props;
   const classes = useStyles(props);
+
+  const loginInputs = [
+    {
+      id: "input0",
+      labelHeading: "Username",
+      ariaLabel: "username",
+      name: "username",
+      type: "text",
+      required: true,
+    },
+    {
+      id: "input1",
+      labelHeading: "Password",
+      ariaLabel: "password",
+      name: "password",
+      type: "password",
+      required: true,
+    },
+  ];
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -33,22 +45,7 @@ const Login = (props) => {
 
   return (
     <Grid spacing={0} className={classes.outerFlexContainer}>
-      <Grid item className={classes.sideBanner}>
-        <div className={classes.bgOverlay}>
-          <img
-            className={classes.bgImg}
-            src={backgroundImg}
-            alt="A group conversing"
-          />
-          <Box className={classes.content}>
-            <ChatLogo />
-            <Typography className={classes.sloganText}>
-              Converse with anyone with any language
-            </Typography>
-          </Box>
-        </div>
-      </Grid>
-
+      <SideBanner />
       <Grid item className={classes.rightSection}>
         <Box>
           <Grid container item className={classes.topContainer}>
@@ -64,49 +61,12 @@ const Login = (props) => {
               Register
             </Button>
           </Grid>
-          <Grid container className={classes.formContainer}>
-            <Typography className={classes.header}>Welcome back!</Typography>
-            <form className={classes.form} onSubmit={handleLogin}>
-              <Grid>
-                <Grid>
-                  <FormControl className={classes.formCtrl} required>
-                    <Typography className={classes.smGrayText}>
-                      Username
-                    </Typography>
-                    <TextField
-                      className={classes.textField}
-                      aria-label="username"
-                      name="username"
-                      type="text"
-                    />
-                  </FormControl>
-                </Grid>
-                <FormControl className={classes.formCtrl} required>
-                  <Typography className={classes.smGrayText}>
-                    Password
-                  </Typography>
-                  <TextField
-                    className={classes.textField}
-                    aria-label="password"
-                    type="password"
-                    name="password"
-                  />
-                </FormControl>
-                <Grid>
-                  <Box className={classes.btnContainer}>
-                    <Button
-                      className={classes.button}
-                      type="submit"
-                      variant="contained"
-                      size="large"
-                      color="primary">
-                      Login
-                    </Button>
-                  </Box>
-                </Grid>
-              </Grid>
-            </form>
-          </Grid>
+          <Form
+            onSubmit={handleLogin}
+            inputs={loginInputs}
+            greetingText="Welcome!"
+            buttonText="Login"
+          />
         </Box>
       </Grid>
     </Grid>
--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,25 +1,58 @@
 import React, { useState } from "react";
 import { Redirect, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
-import {
-  Grid,
-  Box,
-  Typography,
-  Button,
-  FormControl,
-  TextField,
-  FormHelperText,
-} from "@material-ui/core";
+import { Grid, Box, Typography, Button } from "@material-ui/core";
 import { register } from "./store/utils/thunkCreators";
 import { useStyles } from "./styles/styles";
-import backgroundImg from "./assets/bg-img.png";
-import { ReactComponent as ChatLogo } from "./assets/bubble.svg";
+import { Form } from "./components/Form";
+import { SideBanner } from "./components/SideBanner";
 
 const Login = (props) => {
   const history = useHistory();
   const { user, register } = props;
   const [formErrorMessage, setFormErrorMessage] = useState({});
   const classes = useStyles(props);
+
+  const signupInputs = [
+    {
+      id: "input0",
+      labelHeading: "Username *",
+      ariaLabel: "username",
+      name: "username",
+      type: "text",
+      required: true,
+    },
+    {
+      id: "input1",
+      labelHeading: "E-mail Address *",
+      ariaLabel: "e-mail address",
+      name: "email",
+      type: "email",
+      required: true,
+    },
+    {
+      id: "input2",
+      labelHeading: "Password *",
+      ariaLabel: "password",
+      name: "password",
+      type: "password",
+      inputProps: { minLength: 6 },
+      required: true,
+      formHelperText: formErrorMessage.confirmPassword,
+      error: !!formErrorMessage.confirmPassword,
+    },
+    {
+      id: "input3",
+      labelHeading: "Confirm Password *",
+      ariaLabel: "confirm password",
+      name: "confirmPassword",
+      type: "password",
+      inputProps: { minLength: 6 },
+      required: true,
+      formHelperText: formErrorMessage.confirmPassword,
+      error: !!formErrorMessage.confirmPassword,
+    },
+  ];
 
   const handleRegister = async (event) => {
     event.preventDefault();
@@ -43,21 +76,7 @@ const Login = (props) => {
   return (
     <>
       <Grid spacing={0} className={classes.outerFlexContainer}>
-        <Grid item className={classes.sideBanner}>
-          <div className={classes.bgOverlay}>
-            <img
-              className={classes.bgImg}
-              src={backgroundImg}
-              alt="A group conversing"
-            />
-            <Box className={classes.content}>
-              <ChatLogo />
-              <Typography className={classes.sloganText}>
-                Converse with anyone with any language
-              </Typography>
-            </Box>
-          </div>
-        </Grid>
+        <SideBanner />
         <Grid item className={classes.rightSection}>
           <Box>
             <Grid container item className={classes.topContainer}>
@@ -73,94 +92,12 @@ const Login = (props) => {
                 Login
               </Button>
             </Grid>
-
-            <Grid container className={classes.formContainer}>
-              <Typography className={classes.header}>
-                Create an account.
-              </Typography>
-              <form className={classes.form} onSubmit={handleRegister}>
-                <Grid>
-                  <Grid>
-                    <FormControl className={classes.formCtrl}>
-                      <Typography className={classes.smGrayText}>
-                        Username *
-                      </Typography>
-                      <TextField
-                        className={classes.textField}
-                        aria-label="username"
-                        name="username"
-                        type="text"
-                        required
-                      />
-                    </FormControl>
-                  </Grid>
-                  <Grid>
-                    <FormControl className={classes.formCtrl}>
-                      <Typography className={classes.smGrayText}>
-                        E-mail address *
-                      </Typography>
-                      <TextField
-                        className={classes.textField}
-                        aria-label="e-mail address"
-                        type="email"
-                        name="email"
-                        required
-                      />
-                    </FormControl>
-                  </Grid>
-                  <Grid>
-                    <FormControl
-                      className={classes.formCtrl}
-                      error={!!formErrorMessage.confirmPassword}>
-                      <Typography className={classes.smGrayText}>
-                        Password *
-                      </Typography>
-                      <TextField
-                        className={classes.textField}
-                        aria-label="password"
-                        type="password"
-                        inputProps={{ minLength: 6 }}
-                        name="password"
-                        required
-                      />
-                      <FormHelperText>
-                        {formErrorMessage.confirmPassword}
-                      </FormHelperText>
-                    </FormControl>
-                  </Grid>
-                  <Grid>
-                    <FormControl
-                      className={classes.formCtrl}
-                      error={!!formErrorMessage.confirmPassword}>
-                      <Typography className={classes.smGrayText}>
-                        Confirm Password *
-                      </Typography>
-                      <TextField
-                        className={classes.textField}
-                        aria-label="confirm password"
-                        type="password"
-                        inputProps={{ minLength: 6 }}
-                        name="confirmPassword"
-                        required
-                      />
-                      <FormHelperText>
-                        {formErrorMessage.confirmPassword}
-                      </FormHelperText>
-                    </FormControl>
-                  </Grid>
-                  <Box className={classes.btnContainer}>
-                    <Button
-                      type="submit"
-                      color="primary"
-                      variant="contained"
-                      size="large"
-                      className={classes.button}>
-                      Create
-                    </Button>
-                  </Box>
-                </Grid>
-              </form>
-            </Grid>
+            <Form
+              onSubmit={handleRegister}
+              inputs={signupInputs}
+              buttonText="Create"
+              greetingText="Create an account."
+            />
           </Box>
         </Grid>
       </Grid>
