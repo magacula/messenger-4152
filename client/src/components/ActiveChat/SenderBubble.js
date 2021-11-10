972f3f5391f0c9182f1566ab8b5b68d5c4880988
magacula <makoigacula@gmail.com>
2021-11-09 23:07:34 -0800

    updated message bubbles to update images to UI

--- client/src/components/ActiveChat/SenderBubble.js
+++ client/src/components/ActiveChat/SenderBubble.js
@@ -1,38 +1,43 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Box, Typography } from "@material-ui/core";
+import { Image } from "./Image";
 
 const useStyles = makeStyles(() => ({
   root: {
     display: "flex",
     flexDirection: "column",
-    alignItems: "flex-end"
+    alignItems: "flex-end",
   },
   date: {
     fontSize: 11,
     color: "#BECCE2",
     fontWeight: "bold",
-    marginBottom: 5
+    marginBottom: 5,
   },
   text: {
     fontSize: 14,
     color: "#91A3C0",
     letterSpacing: -0.2,
     padding: 8,
-    fontWeight: "bold"
+    fontWeight: "bold",
+    textAlign: "center",
   },
   bubble: {
     background: "#F4F6FA",
-    borderRadius: "10px 10px 0 10px"
-  }
+    borderRadius: "10px 10px 0 10px",
+  },
 }));
 
 const SenderBubble = (props) => {
   const classes = useStyles();
-  const { time, text } = props;
+  const { time, text, attachments } = props;
   return (
     <Box className={classes.root}>
       <Typography className={classes.date}>{time}</Typography>
+      {/* When we want to send images in our message */}
+      {attachments !== null && <Image attachments={attachments} />}
+
       <Box className={classes.bubble}>
         <Typography className={classes.text}>{text}</Typography>
       </Box>
